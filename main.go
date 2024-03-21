da3ded9bbbb57b47cac7bd2a893c6b035a1a49fe
Boukabouya <boukabouyarayane2019@gmail.com>
2024-03-21 11:48:55 +0100

    variables without inisialisation have a default values in Golang

--- main.go
+++ main.go
@@ -11,8 +11,8 @@ func main() {
 	fmt.Println(rand.Int())
 	fmt.Printf("The squar is %g that's all \n", math.Sqrt(9))
 }*/
-// variables
-
+// variables exemple 01
+/*
 import (
 	"fmt"
 	"math/cmplx"
@@ -26,4 +26,19 @@ func main() {
 	var b bool = true
 	var c complex128 = cmplx.Sqrt(5 + 12i)
 	fmt.Println(i, f, f2, s, b, c)
+}*/
+// Variables exemple 02
+import (
+	"fmt"
+)
+
+func main() {
+	var i int
+	/*i = 5
+	i = 27
+	fmt.Println("\n The value  of i is ", i)//The value  of i is  27*/
+	var f float32
+	var s string
+	var b bool
+	fmt.Printf("%v %v %q %v\n", i, f, s, b) //%v : default print , %q  : for string with quote
 }
