c62b280913605b12f31d7530c7ff2063e9aa903f
Boukabouya <boukabouyarayane2019@gmail.com>
2024-03-21 10:44:33 +0100

    Part 1 basics

---
+++ main.go
@@ -0,0 +1,14 @@
+package main
+
+/*import (
+	"fmt"
+	"math"
+	"math/rand"
+)
+
+func main() {
+	fmt.Println("Hello world")
+	fmt.Println(rand.Int())
+	fmt.Printf("The squar is %g that's all \n", math.Sqrt(9))
+}*/
+// variables
