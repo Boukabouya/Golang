7c22fb2fdb4c967cf3e01ed8913a9a63e19cc585
Boukabouya <boukabouyarayane2019@gmail.com>
2024-03-21 11:10:37 +0100

    variables in golang

--- main.go
+++ main.go
@@ -12,3 +12,18 @@ func main() {
 	fmt.Printf("The squar is %g that's all \n", math.Sqrt(9))
 }*/
 // variables
+
+import (
+	"fmt"
+	"math/cmplx"
+)
+
+func main() {
+	var i int = 1
+	var f float32 = 2.75
+	var f2 float64 = 2.758846
+	var s string = "Hello go"
+	var b bool = true
+	var c complex128 = cmplx.Sqrt(5 + 12i)
+	fmt.Println(i, f, f2, s, b, c)
+}
