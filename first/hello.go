e44bee6ab85f1e5647eb09081720f95f329e5cb2
Boukabouya <boukabouyarayane2019@gmail.com>
2024-03-27 14:22:09 +0100

    add comment

--- first/hello.go
+++ first/hello.go
@@ -4,6 +4,6 @@ import "fmt"
 
 // start entry point
 func main() {
-	fmt.Println("Hello from amina rayene")
+	fmt.Println("Hello from amina rayene") // with seiclon is the valid syntax for golang
 
 }
